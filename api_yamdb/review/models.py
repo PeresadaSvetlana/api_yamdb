0d5185660742dbe4224617ecedcfff2d91bb0623
Polina <malinamalino@mail.ru>
2022-04-14 19:40:09 +0300

    2

--- api_yamdb/review/models.py
+++ api_yamdb/review/models.py
@@ -1,18 +1,76 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from models import User
 
-USER = 'User'
-ROLE_CHOICES = [
-        ('USER', 'User'),
-        ('ADMIN', 'Admin'),
-        ('MODERATOR', 'Moderator')
-    ]
 
-class User(AbstractUser):
-    username = models.ForeignKey(blank=False) 
-    email = models.EmailField(blank=False)
-    role = models.CharField(choices=ROLE_CHOICES,
-        default=USER)
-    bio = models.TextField('Биография', blank=True) 
-    first_name = models.CharField(max_length=20, blank=True)
-    last_name = models.CharField(max_length=20, blank=True)
+class Categories(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Наименование категории'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Наименование жанра'
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+        verbose_name='Название'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Наименование'
+    )
+    year = models.IntegerField()
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание')
+    genre = models.ManyToManyField(
+        Genres,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='api',
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='api',
+        verbose_name='Жанр'
+    )
+
+    def __str__(self):
+        return self.name
+
+class Comments(models.Model):
+    review_id = ?
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+class Review(models.Model):
+    title_id = ?
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='review')
+    score = models.AutoField(related_name='score')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+USER = 'User'
+ROLE_CHOICES = [
+        ('USER', 'User'),
+        ('ADMIN', 'Admin'),
+        ('MODERATOR', 'Moderator')
+    ]
+
+class User(AbstractUser):
+    username = models.ForeignKey(blank=False) 
+    email = models.EmailField(blank=False)
+    role = models.CharField(choices=ROLE_CHOICES,
+        default=USER)
+    bio = models.TextField('Биография', blank=True) 
+    first_name = models.CharField(max_length=20, blank=True)
+    last_name = models.CharField(max_length=20, blank=True)
