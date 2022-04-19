0f8842a4639550664bea8764c4c60301a9639121
Polina <malinamalino@mail.ru>
2022-04-19 21:21:06 +0300

    4

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -55,8 +55,12 @@ class TitleWriteSerializer(TitleSerializer):
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = '__all__'
         model = User
+        fields = (
+            'username', 'email',
+            'first_name', 'last_name',
+            'bio', 'role'
+        )
 
 
 class SignUpSerializer(serializers.ModelSerializer):
