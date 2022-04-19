fa631a39df0c79e0f2bb89c9b90b166469c8413e
Polina <malinamalino@mail.ru>
2022-04-19 22:26:41 +0300

    2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from review.models import Category, Genre, Title, User, Review
 from rest_framework import mixins
-
+from rest_framework.permissions import IsAuthenticated
 from .serializers import (CategorySerializer, GenreSerializer,
                           ObtainTokenSerializer, SignUpSerializer,
                           TitleSerializer, UserSerializer, CommentSerializer,
@@ -18,7 +18,7 @@ from .serializers import (CategorySerializer, GenreSerializer,
                           ReviewSerializer)
 
 from .filters import TitleFilter
-from .permissions import (IsAdminOrReadOnly,
+from .permissions import (IsAdminOrReadOnly, IsAdminOrSuperOnly,
                           IsAdminModeratorAuthororReadOnly, IsAdmin)
 
 
@@ -68,6 +68,19 @@ class UserViewSet(viewsets.ModelViewSet):
     search_fields = ('username',)
     lookup_field = 'username'
 
+    @action(methods=['post'],
+            detail=False,
+            permission_classes=(IsAuthenticated, IsAdminOrSuperOnly,))
+    def create_user(self, request):
+        serializer = UserSerializerReadOnly(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
     @action(methods=['GET', 'PATCH'],
             detail=False,
             permission_classes=[permissions.IsAuthenticated],
@@ -81,7 +94,7 @@ class UserViewSet(viewsets.ModelViewSet):
             )
             serializer.is_valid()
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
